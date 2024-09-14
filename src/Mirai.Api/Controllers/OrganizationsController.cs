e617838cc391db8a08230bf6f21af19a7e6b2539
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-09-14 00:19:49 -0300

    feat: add swagger documentation

--- src/Mirai.Api/Controllers/OrganizationsController.cs
+++ src/Mirai.Api/Controllers/OrganizationsController.cs
@@ -13,6 +13,11 @@ namespace Mirai.Api.Controllers;
 [AllowAnonymous]
 public class OrganizationsController(ISender _mediator) : ApiController
 {
+    /// <summary>
+    /// Create a new organization.
+    /// </summary>
+    /// <param name="request">The request to create a new organization.</param>
+    /// <returns>The newly created organization.</returns>
     [HttpPost(ApiEndpoints.Organizations.Create)]
     [ProducesResponseType(typeof(OrganizationResponse), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -30,6 +35,11 @@ public class OrganizationsController(ISender _mediator) : ApiController
             Problem);
     }
 
+    /// <summary>
+    /// Get an organization by its ID.
+    /// </summary>
+    /// <param name="organizationId">The ID of the organization to get.</param>
+    /// <returns>The organization with the specified ID.</returns>
     [HttpGet(ApiEndpoints.Organizations.Get)]
     [ProducesResponseType(typeof(IEnumerable<OrganizationResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -44,6 +54,10 @@ public class OrganizationsController(ISender _mediator) : ApiController
             Problem);
     }
 
+    /// <summary>
+    /// List all organizations.
+    /// </summary>
+    /// <returns>All organizations.</returns>
     [HttpGet(ApiEndpoints.Organizations.List)]
     [ProducesResponseType(typeof(IEnumerable<OrganizationResponse>), StatusCodes.Status200OK)]
     public async Task<IActionResult> ListOrganizations()
@@ -57,6 +71,12 @@ public class OrganizationsController(ISender _mediator) : ApiController
             Problem);
     }
 
+    /// <summary>
+    /// Update an organization.
+    /// </summary>
+    /// <param name="organizationId">The ID of the organization to update.</param>
+    /// <param name="request">The request to update the organization.</param>
+    /// <returns>The updated organization.</returns>
     [HttpPut(ApiEndpoints.Organizations.Update)]
     [ProducesResponseType(typeof(OrganizationResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
--- src/Mirai.Api/DependencyInjection.cs
+++ src/Mirai.Api/DependencyInjection.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 namespace Mirai.Api;
 
 public static class DependencyInjection
@@ -8,6 +11,45 @@ public static class DependencyInjection
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
         services.AddProblemDetails();
+
+        return services;
+    }
+
+    public static IServiceCollection AddSwaggerGen(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Mirai.Api",
+                Version = "v1",
+                Description = "Mirai (Japanese word for \"future\" and \"forward-thinking\") is a project management tool that aims to help teams collaborate and manage their projects more effectively.",
+            });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer",
+                        },
+                    },
+                    Array.Empty<string>()
+                },
+            });
+
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
 
         return services;
     }
